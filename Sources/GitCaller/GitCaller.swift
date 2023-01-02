87871765c7a7d7d686bb127e570408617e11a98e
Artur Hellmann <artur.hellmann@luca-app.de>
2023-01-02 19:09:19 +0100

    Added an async option for calls

--- Sources/GitCaller/GitCaller.swift
+++ Sources/GitCaller/GitCaller.swift
@@ -1,72 +1,102 @@
 import Cocoa
 import Combine
 
-struct GitCaller {
+private func runTask(command: String, onReceive: ((String) -> Void)?, onCompletion: (() -> Void)?) -> String {
+    let task = Process()
     
-    // MARK: Private
-    fileprivate static func run(command: String) -> some Publisher<String, Never> {
-        
-        let resultPublisher = PassthroughSubject<String, Never>()
-        
-        let task = Process()
-        
-        task.arguments = ["-c", command]
-        task.launchPath = "/bin/zsh"
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
 
-        let pipe = Pipe()
-        let errorPipe = Pipe()
-        task.standardOutput = pipe
-        task.standardError = errorPipe
-        let outHandle = pipe.fileHandleForReading
-        let errorHandle = errorPipe.fileHandleForReading
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.standardError = pipe
+    let outHandle = pipe.fileHandleForReading
+
+    if let onReceive = onReceive, let onCompletion = onCompletion {
         outHandle.waitForDataInBackgroundAndNotify()
-        errorHandle.waitForDataInBackgroundAndNotify()
 
         var updateObserver: NSObjectProtocol!
         updateObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable, object: outHandle, queue: nil, using: { notification in
             let data = outHandle.availableData
             if !data.isEmpty {
                 if let str = String(data: data, encoding: .utf8) {
-                    resultPublisher.send(str)
+                    onReceive(str)
                 }
                 outHandle.waitForDataInBackgroundAndNotify()
             } else {
-                resultPublisher.send(completion: .finished)
                 NotificationCenter.default.removeObserver(updateObserver!)
-            }
-        })
-        
-        var errorObserver: NSObjectProtocol!
-        errorObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable, object: errorHandle, queue: nil, using: { notification in
-            let data = errorHandle.availableData
-            if !data.isEmpty {
-                if let str = String(data: data, encoding: .utf8) {
-                    resultPublisher.send(str)
-                }
-                errorHandle.waitForDataInBackgroundAndNotify()
-            } else {
-                NotificationCenter.default.removeObserver(errorObserver!)
+                onCompletion()
             }
         })
         
         var taskObserver : NSObjectProtocol!
         taskObserver = NotificationCenter.default.addObserver(forName: Process.didTerminateNotification, object: task, queue: nil, using: { notification in
-            print("terminated")
             NotificationCenter.default.removeObserver(taskObserver!)
         })
+    }
 
-        task.launch()
-        
-        return resultPublisher
+    task.launch()
+    
+    if onReceive == nil || onCompletion == nil {
+        return String(data: outHandle.readDataToEndOfFile(), encoding: .utf8) ?? ""
+    } else {
+        return ""
     }
     
-    actor DataHolder {
-        var data: Data? = nil
-        
-        func setData(data: Data) {
-            self.data = data
-        }
+}
+
+public struct GitCall: Publisher {
+    public typealias Output = String
+    public typealias Failure = Never
+    
+    let command: String
+    
+    init(command: String) {
+        self.command = command
     }
+    
+    public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, String == S.Input {
+        let subscription = GitCallSubscription(command: command, subscriber: subscriber)
+        subscriber.receive(subscription: subscription)
+    }
+    
+    
+    final class GitCallSubscription<S: Subscriber>: Cancellable, Subscription where S.Input == Output, S.Failure == Failure {
+        private let command: String
+        private var subscriber: S?
+        
+        init(command: String, subscriber: S) {
+            self.command = command
+            self.subscriber = subscriber
+        }
+        
+        func request(_ demand: Subscribers.Demand) {
+            _ = runTask(command: command, onReceive: { [weak self] result in
+                _ = self?.subscriber?.receive(result)
+            }, onCompletion: { [weak self] in
+                self?.subscriber?.receive(completion: .finished)
+            })
+        }
+        
+        func cancel() {
+            subscriber = nil
+        }
+        
+    }
+    
+}
+
+struct GitCaller {
+    
+    // MARK: Private
+    fileprivate static func run(command: String) -> some Publisher<String, Never> {
+        return GitCall(command: command)
+    }
+    
+    fileprivate static func runAsync(command: String) async -> String {
+        return runTask(command: command, onReceive: nil, onCompletion: nil)
+    }
+    
 }
 
 extension CommandSpec {
@@ -79,5 +109,10 @@ extension CommandSpec {
                 lastResult + nextResult
             })
             .eraseToAnyPublisher()
+    }
+    
+    /// Provides an async await style run method.
+    public func runAsync() async throws -> String {
+        return await GitCaller.runAsync(command: self.resolve())
     }
 }
